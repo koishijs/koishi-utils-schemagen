6ac8d877bc9bbeb7ed88d0662bbd8d7507571f85
purerosefallen <78877@qq.com>
2021-10-23 17:12:15 +0800

    don't use symbol

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const SchemaMetaKey = Symbol('_koishi_schema_meta');
-export const SchemaKeysMetaKey = Symbol('_koishi_schemakeys_meta');
-export const SchemaClassKey = Symbol('_koishi_schema_class_meta');
+export const SchemaMetaKey = '_koishi_schema_meta';
+export const SchemaKeysMetaKey = '_koishi_schemakeys_meta';
+export const SchemaClassKey = '_koishi_schema_class_meta';
