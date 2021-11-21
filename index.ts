7bae8ce5181b99c8f322a2f5e5679fee6d15ea72
purerosefallen <78877@qq.com>
2021-11-21 16:48:05 +0800

    now its just stub

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
-export * from './src/def';
-export * from './src/decorators';
-export * from './src/methods';
-export * from './src/constants';
+import { ClassType, RegisterSchema } from 'schemastery-gen';
+import Schema from 'schemastery';
+export * from 'schemastery-gen';
+
+export function schemaFromClass<T>(cl: ClassType<T>): Schema<T> {
+  if ((cl as Schema<T>).type) {
+    return cl as Schema<T>;
+  }
+  return RegisterSchema()(cl);
+}
+
+export function schemaTransform<T>(cl: ClassType<T>, obj: any): T {
+  return new cl(obj);
+}
--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SchemaMetaKey = '_koishi_schema_meta';
-export const SchemaKeysMetaKey = '_koishi_schemakeys_meta';
-export const SchemaClassKey = '_koishi_schema_class_meta';
